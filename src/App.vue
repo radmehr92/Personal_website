aadfa5eff7f85f1e9caa5b3db3aeec14b126ff72
alirad <ali.radmeehr@gmail.com>
2025-09-22 11:52:39 +0330

    PT

--- src/App.vue
+++ src/App.vue
@@ -97,7 +97,7 @@
                   </div>
 
                   <div class="flex rounded-full border border-sky-600/50 w-72 h-72 shadow-md z-10 animate-fade-up animate-once animate-ease-in-out">
-                    <img class="rounded-full border border-sky-600/50 w-72 h-72" src="/src/assets/image/profile.jpg" alt="Ali Radmehr" />
+                    <img class="rounded-full border border-sky-600/50 w-72 h-72" src="/src/assets/image/ali.png" alt="Ali Radmehr" />
                   </div>
 
 
@@ -219,7 +219,7 @@
                   </div>
 
                   <div class="flex rounded-full border border-sky-600/50 w-72 h-72 shadow-md z-10 animate-fade-up animate-once animate-ease-in-out">
-                    <img class="rounded-full border border-sky-600/50 w-72 h-72" src="/src/assets/image/profile.jpg" alt="Ali Radmehr" />
+                    <img class="rounded-full border border-sky-600/50 w-72 h-72" src="/src/assets/image/ali.png" alt="Ali Radmehr" />
                   </div>
 
 
@@ -374,7 +374,7 @@
                     </div>
 
                     <div class="flex rounded-full border border-sky-600/50 w-72 h-72 shadow-md z-10 animate-fade-up animate-once animate-ease-in-out">
-                      <img class="rounded-full border border-sky-600/50 w-72 h-72" src="/src/assets/image/profile.jpg" alt="Ali Radmehr" />
+                      <img class="rounded-full border border-sky-600/50 w-72 h-72" src="/src/assets/image/ali.png" alt="Ali Radmehr" />
                     </div>
 
 
@@ -496,7 +496,7 @@
                     </div>
 
                     <div class="flex rounded-full border border-sky-600/50 w-72 h-72 shadow-md z-10 animate-fade-up animate-once animate-ease-in-out">
-                      <img class="rounded-full border border-sky-600/50 w-72 h-72" src="/src/assets/image/profile.jpg" alt="Ali Radmehr" />
+                      <img class="rounded-full border border-sky-600/50 w-72 h-72" src="/src/assets/image/ali.png" alt="Ali Radmehr" />
                     </div>
 
 
@@ -654,7 +654,7 @@
                     </div>
 
                     <div class="flex rounded-full border border-sky-600/50 w-52 h-52 shadow-md z-10 animate-fade-up animate-once animate-ease-in-out">
-                      <img class="rounded-full border border-sky-600/50 w-52 h-52" src="/src/assets/image/profile.jpg" alt="Ali Radmehr" />
+                      <img class="rounded-full border border-sky-600/50 w-52 h-52" src="/src/assets/image/ali.png" alt="Ali Radmehr" />
                     </div>
 
 
@@ -776,7 +776,7 @@
                     </div>
 
                     <div class="flex rounded-full border border-sky-600/50 w-52 h-52 shadow-md z-10 animate-fade-up animate-once animate-ease-in-out">
-                      <img class="rounded-full border border-sky-600/50 w-52 h-52" src="/src/assets/image/profile.jpg" alt="Ali Radmehr" />
+                      <img class="rounded-full border border-sky-600/50 w-52 h-52" src="/src/assets/image/ali.png" alt="Ali Radmehr" />
                     </div>
 
 
@@ -877,7 +877,7 @@
                   </div>
 
                   <div class="flex rounded-full border border-sky-600/50 w-40 h-40 shadow-md z-10 animate-fade-up animate-once animate-ease-in-out">
-                    <img class="rounded-full border border-sky-600/50 w-40 h-40" src="/src/assets/image/profile.jpg" alt="Ali Radmehr" />
+                    <img class="rounded-full border border-sky-600/50 w-40 h-40" src="/src/assets/image/ali.png" alt="Ali Radmehr" />
                   </div>
 
 
@@ -999,7 +999,7 @@
                   </div>
 
                   <div class="flex rounded-full border ml-72 border-sky-600/50 w-40 h-40 shadow-md z-10 animate-fade-up animate-once animate-ease-in-out">
-                    <img class="rounded-full border border-sky-600/50 w-40 h-40" src="/src/assets/image/profile.jpg" alt="Ali Radmehr" />
+                    <img class="rounded-full border border-sky-600/50 w-40 h-40" src="/src/assets/image/ali.png" alt="Ali Radmehr" />
                   </div>
 
 
@@ -1160,7 +1160,7 @@
                   </div>
 
                   <div class="flex rounded-full border border-sky-600/50 w-40 h-40 shadow-md z-10 animate-fade-up animate-once animate-ease-in-out">
-                    <img class="rounded-full border border-sky-600/50 w-40 h-40" src="/src/assets/image/profile.jpg" alt="Ali Radmehr" />
+                    <img class="rounded-full border border-sky-600/50 w-40 h-40" src="/src/assets/image/ali.png" alt="Ali Radmehr" />
                   </div>
 
 
@@ -1282,7 +1282,7 @@
                   </div>
 
                   <div class="flex rounded-full border mr-80 border-sky-600/50 w-40 h-40 shadow-md z-10 animate-fade-up animate-once animate-ease-in-out">
-                    <img class="rounded-full border border-sky-600/50 w-40 h-40" src="/src/assets/image/profile.jpg" alt="Ali Radmehr" />
+                    <img class="rounded-full border border-sky-600/50 w-40 h-40" src="/src/assets/image/ali.png" alt="Ali Radmehr" />
                   </div>
 
 
