2568429b6dca71c7386793fa7621bfef2ab05cfe
alirad <ali.radmeehr@gmail.com>
2026-05-19 21:20:12 +0330

    p6

--- src/App.vue
+++ src/App.vue
@@ -374,7 +374,7 @@
                     </div>
 
                     <div class="flex rounded-full border border-sky-600/50 w-72 h-72 shadow-md z-10 animate-fade-up animate-once animate-ease-in-out">
-                      <img class="rounded-full border border-sky-600/50 w-72 h-72" src="/src/assets/image/ali.png" alt="Ali Radmehr" />
+                      <img class="rounded-full border border-sky-600/50 w-72 h-72" src="/src/assets/image/a.png" alt="Ali Radmehr" />
                     </div>
 
 
@@ -496,7 +496,7 @@
                     </div>
 
                     <div class="flex rounded-full border border-sky-600/50 w-72 h-72 shadow-md z-10 animate-fade-up animate-once animate-ease-in-out">
-                      <img class="rounded-full border border-sky-600/50 w-72 h-72" src="/src/assets/image/ali.png" alt="Ali Radmehr" />
+                      <img class="rounded-full border border-sky-600/50 w-72 h-72" src="/src/assets/image/a.png" alt="Ali Radmehr" />
                     </div>
 
 
